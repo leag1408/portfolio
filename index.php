602a8026c92c2df68ddd8062066c4735b442a553
leag1408 <leaa_gimenez@hotmail.com>
2020-09-18 18:55:01 -0300

    cambios

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyectos</title>
+    <title>Inicio</title>
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
@@ -12,103 +12,82 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100,200,300,400,500,600,700,800.900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="css/estilos.css">
-    <script src="css/bootstrap/js/bootstrap.min.js"></script>
     <script src="js/jquery.js"></script>
+    <script src="css/bootstrap/js/bootstrap.min.js"></script>
     <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100,200,300,400,500,600,700,800.900&display=swap"
-        rel="stylesheet">
+
 
 </head>
 
-<body id="proyectos">
+<body id="inicio">
     <header>
-        <div class="container">
+        <div class="container-fluid">
             <nav class="navbar navbar-expand-md px-0">
+                <input type="checkbox" id="btn-menu">
+                <label for="btn-menu"></label>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMenu" aria-controls="navMenu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa fa-bars"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item pr-5">
-                            <a class="nav-link" href="index.html">Inicio</a>
+                            <a class="nav-link active" href="index.html">Inicio</a>
                         </li>
                         <li class="nav-item pr-5">
                             <a class="nav-link" href="sobre-mi.html">Sobre mi</a>
                         </li>
                         <li class="nav-item pr-5">
-                            <a class="nav-link active" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link" href="proyectos.html">Proyectos</a>
                         </li>
                         <li class="nav-item pr-5">
                             <a class="nav-link" href="contacto.html">Contacto</a>
                         </li>
                     </ul>
                     <div class="form-inline my-2 my-lg-0">
-                        <a href="" class="btn btn-pdf">DESCARGÁ MI CV <i class="fas fa-download"></i></a>
+                        <a href="" class="btn btn-rojo">DESCARGÁ MI CV <i class="fas fa-download"></i></a>
                     </div>
 
                 </div>
             </nav>
         </div>
     </header>
+
     <main>
         <div class="container">
-            <div class="row">
-                <div class="col-12 mt-5">
-                    <h1>Proyectos</h1>
+            <div class="row justify-content-center inicio">
+                <div class="col-8 texto text-center">
+                    <p class="my-3 mx-2"> Hola mi nombre es Rosario, soy desarrolladora full stack. | <i class="fas fa-search"></i></p>
+                    
                 </div>
             </div>
-            <div class="row">
-                <div class="col-sm-6 col-12 mt-4">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, nam sunt fugit vero delectus a
-                        distinctio obcaecati soluta consectetur sequi necessitatibus similique odit enim maxime aperiam
-                        dicta illum commodi at? </p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm 4 col-12">
-                    <img src="images/proyectos/clientes-09.png">
-                </div>
-                <div class="col-sm 4 col-12">
-                    <img src="images/proyectos/ventas-09.png">
-                </div>
-                <div class="col-sm 4 col-12">
-                    <img src="images/proyectos/proyecto-09.png">
-                </div>
-                <div class="col-sm 4 col-12">
-                    <h3 class="titulo-proyectos">ABM CLIENTES</h3>
-                </div>
-                <div class="col-sm 4 col-12">
-                    <h3 class="titulo-proyectos">SISTEMA DE GESTIÓN DE VENTAS</h3>
-                </div>
-                <div class="col-sm 4 col-12">
-                    <h3 class="titulo-proyectos">PROYECTO INTEGRADOR</h3>
+            <div class="row justify-content-center">
+                <div class="text-right">
+                    <button type="submit" class="btn shadow mt-3">MIRÁ MI TRABAJO</button>
                 </div>
             </div>
         </div>
-
-
-        </div>
-
-
     </main>
+
+
     <footer>
         <div class="container">
-            <div class="row">
-                <div class="col-3">
+            <div class="row mt-5">
+                <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
                     <a href="https://github.com/" target="_blank" title="Github"><i class="fab fa-github mr-1"></i></a>
                     <a href="https://www.linkedin.com/in/leagimenez44/" target="_blank" title="Linkedin"><i
                             class="fab fa-linkedin-in"></i></a>
                 </div>
-                <div class="col-3">
+                <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
                     Sponsor <a href="htpps://depcsuite.com" target="_blank" title="DEPCSUITE">DePc Suite </a>
                 </div>
-                <div class="col-3">
+                <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
                     <a href="mailto:info@depcsuite.com">info@depcsuite.com</a>
                 </div>
-                <div class="col-3 text-right">
+                <div class="col-sm-3 col-12 text-right">
                     <a href="https://api.whatsapp.com/send?phone=541168902210" target="_blank" title="Whatsapp"><i
                             class="fab fa-whatsapp"></i></a>
-
                 </div>
-
             </div>
         </div>
     </footer>
